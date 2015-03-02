c7e7189987e8c658f66f32c35e8696348dcb96b2
Chun-Wei Yuan <CW@chun-wei-yuans-macbook-2.local>
2015-03-01 22:23:36 -0800

    renamed repo, updated readme, as well as added plots to ipython notebook.

---
+++ weather/README.md
@@ -0,0 +1,30 @@
+## Intro
+
+This is essentially a python code written to predict the max temperature of 500 locations, for everyday over the entire year of 2010.  
+
+## Neural Net (neural_weather.py)
+
+The code would first read in temperatures of the 500 locations, for everyday from 1980 to 2009, from the file "weather_data.csv".  These data will then be used to train/cross-validate/test an artificial neural network (one separate network for every location) to predict the 2010 temperatures.  Outputs are written to output.csv.  
+
+It's entirely questionable why anyone would use a neural net for this particular machine learning task.  In the end, it's just a fun project with interesting data constraints that combines neural network, optimization of network parameters via cross-validation, and making predictions.  In addition, it's very parallelizable, so it's a nice starting point to test out Spark.
+
+To run the code, simply type
+
+```
+python neural_weather.py
+```
+
+To run on Spark (locally, with 2 cores), 
+
+```
+MASTER=local[2] /usr/local/bin/spark/bin/spark-submit neural_weather.py
+```
+
+One can change the number_of_locations in the code to as high as 500.  Also, cross-validation/test errors are computed in the object, but not exported.
+
+
+## Kalman Filter (kalman_weather.py)
+
+The Kalman filter implemented here assumes there's no control vector; as such, it's used essentially as a noise filtering technique (see the ipython notebook in this repo).  One can run the filter through the temperature measurements for each location first, before feeding the filtered data into the neural network for training/prediction.
+
+Alternatively, one could also assume that the underlying state is stationary, and everything observed is just noise.  Then the prediction for next year's max temperature, for a given location and date, is just the current tempearture state is that location/date.
--- weather/kalman_weather.py
+++ weather/kalman_weather.py
@@ -4,15 +4,10 @@ from kalman import Kalman1D
 ###-------------------------------------------------------------------------------------
 class KalmanWeather(PandasMaster):
 
-      def __init__(self, file='weather_data.csv', number_of_locations=5, pfile='predict.csv', xfile='xv.csv', tfile='test.csv'): # number of locations is an input
+      def __init__(self, file='weather_data.csv', number_of_locations=5, 
+                   pfile='predict.csv', xfile='xv.csv', tfile='test.csv'): # number of locations is an input
           # Sets up the data frame.  The locations are labeled from 1 to 500
           super(KalmanWeather, self).__init__(file, number_of_locations, pfile, xfile, tfile)
-
-
-      def prediction_error(self, network, data_set):
-          # this routine tabulates the absolute difference between the predictions and actuals from cross validation (or test)
-          errors = [numpy.abs(data[1][0]-network.update(data[0])[0]) for data in data_set]
-          return float( numpy.mean(errors) )
 
 
       def learn(self, location, month, day):
@@ -20,9 +15,11 @@ class KalmanWeather(PandasMaster):
           # The state is hence the predicted value.
           tr_set, xv_set, ts_set = self.get_sets(location, month, day, n_prior_years=0)
           data_set = tr_set + xv_set + ts_set  # I actually want all data strung together to improve the filter
+          years, temperatures = self.day_history(location, month, day)
+          E = float(max(temperatures) - min(temperatures)) / 2.0 # approximate the noise covariance
           kalman = Kalman1D( A = numpy.matrix([1]), B = numpy.matrix([0]), H = numpy.matrix([1]),
-                             x0 = numpy.matrix(data_set[0][1]), P0 = numpy.matrix([5]),
-                             Q = numpy.matrix([5]), R = numpy.matrix([5]) ) # initialize the filter
+                             x0 = numpy.matrix(data_set[0][1]), P0 = numpy.matrix([E]),
+                             Q = numpy.matrix([E]), R = numpy.matrix([E]) ) # initialize the filter
           u = numpy.matrix([0]) # the control vector is 0 in this case
           for data in data_set: # if n_prior_years = 30, this should really just be one 
               kalman.update(u, numpy.matrix(data[1]))
